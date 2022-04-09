a55a02e8120ed3c45312ecad854b5b24d5fe71cc
Mark Raynsford <code@io7m.com>
2022-04-09 19:52:48 +0000

    Update URLs

--- com.io7m.jwheatsheaf.filter.glob/src/main/java/com/io7m/jwheatsheaf/filter/glob/JWFilterGlobFactory.java
+++ com.io7m.jwheatsheaf.filter.glob/src/main/java/com/io7m/jwheatsheaf/filter/glob/JWFilterGlobFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021 Mark Raynsford <code@io7m.com> http://io7m.com
+ * Copyright © 2021 Mark Raynsford <code@io7m.com> https://www.io7m.com
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
